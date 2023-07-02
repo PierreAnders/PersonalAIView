2e134903dc6808e36285f0665e3d0a3d5b91dc48
PierreAnders <pierre.untas@gmail.com>
2023-07-03 00:28:49 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="query" @keyup.enter="sendQuery">
+    <p>{{ response }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      query: '',
+      response: '',
+    };
+  },
+  methods: {
+    async sendQuery() {
+      const res = await axios.post('http://localhost:5000/chat', {
+        query: this.query,
+      });
+      this.response = res.data;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
